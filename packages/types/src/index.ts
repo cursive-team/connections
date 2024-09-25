5264fe81e23a11e247045c4b77947a2b0c8d4a1a
AndrewCLu <andrewclu98@gmail.com>
2024-09-25 00:28:39 -0400

    add zod validation to backend and frontend

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,19 @@
 export * from "./user";
+import { ZodError } from "zod";
+import { fromZodError } from "zod-validation-error";
 
 export interface ErrorResponse {
   error: string;
+}
+
+export function errorToString(error: unknown): string {
+  if (error instanceof ZodError) {
+    return fromZodError(error).message;
+  } else if (error instanceof Error) {
+    return error.message;
+  } else if (typeof error === "string") {
+    return error;
+  } else {
+    return "An unknown error has occurred";
+  }
 }
