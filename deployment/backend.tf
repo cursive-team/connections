cb7718b885a528dfe38e8c1d687c2c09ffd42171
Steven Elleman <steven.elleman@gmail.com>
2024-10-05 14:50:26 -0700

    update tfstate path (#44)

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "connections-tfstate" # name of the s3 bucket that was manually created
-    key     = "terraform.tfstate"
+    key     = ".terraform/terraform.tfstate"
     region  = "ap-southeast-1"
     encrypt = true
 
