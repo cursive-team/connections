99b4faded2eb076cc0a8685071e351d8713fa2e5
Andrew Lu <andrewclu98@gmail.com>
2024-10-09 05:59:59 +0100

    fix script types (#53)

--- apps/backend/src/scripts/lanna/loadChips.ts
+++ apps/backend/src/scripts/lanna/loadChips.ts
@@ -1,24 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-import { EDGE_CITY_LANNA_CHIPS } from "./chips";
+// import { EDGE_CITY_LANNA_CHIPS } from "./chips";
 
 const prisma = new PrismaClient();
 
 async function loadChips() {
   try {
     console.log("Starting to load chips...");
-
-    const chipsToCreate = EDGE_CITY_LANNA_CHIPS.map((chipUrl) => {
-      const uid = new URL(chipUrl).searchParams.get("uid");
-      if (!uid) {
-        console.error(`Invalid chip URL: ${chipUrl}`);
-        return null;
-      }
-      return {
-        chipId: uid,
-        chipIssuer: "EDGE_CITY_LANNA",
-        chipVariant: "NTAG212",
-      };
-    }).filter((chip) => chip !== null);
+    // const chipSource: string[] = EDGE_CITY_LANNA_CHIPS;
+    const chipSource: string[] = [];
+    const chipsToCreate = chipSource
+      .map((chipUrl) => {
+        const uid = new URL(chipUrl).searchParams.get("uid");
+        if (!uid) {
+          console.error(`Invalid chip URL: ${chipUrl}`);
+          return null;
+        }
+        return {
+          chipId: uid,
+          chipIssuer: "EDGE_CITY_LANNA",
+          chipVariant: "NTAG212",
+        };
+      })
+      .filter((chip) => chip !== null);
 
     await prisma.chip.createMany({
       data: chipsToCreate,
