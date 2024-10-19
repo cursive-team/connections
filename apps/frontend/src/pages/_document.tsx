47e2744d942b1dcf78ad92a4b0dec02ab35879ff
Kalidou Diagne <kld.diagne@gmail.com>
2024-10-19 02:11:20 +0100

    update favicon

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <body className="antialiased">
         <Main />
