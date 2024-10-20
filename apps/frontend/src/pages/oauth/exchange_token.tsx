075605ff1a7b37d49cd80e16185227af62465d3d
Steven Elleman <steven.elleman@gmail.com>
2024-10-20 12:52:42 +0700

    after getting oauth token, import data and convert to leaderboard entry

--- apps/frontend/src/pages/oauth/exchange_token.tsx
+++ apps/frontend/src/pages/oauth/exchange_token.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from "react";
 import {useRouter} from "next/router";
-import {mintOAuthToken} from "@/lib/oauth";
+import {importOAuthData, mintOAuthToken} from "@/lib/oauth";
 import {toast} from "sonner";
 import {CursiveLogo} from "@/components/ui/HeaderCover";
-import {AccessToken} from "@types";
+import {AccessToken, ChipIssuer} from "@types";
 import {saveAccessToken} from "@/lib/storage/localStorage/oauth";
+import {OAUTH_APP_MAPPING} from "@/config";
+import {storage} from "@/lib/storage";
 
 const OAuthAccessTokenPage: React.FC = () => {
   const router = useRouter();
@@ -13,18 +15,39 @@ const OAuthAccessTokenPage: React.FC = () => {
     const fetchAccessToken = async () => {
       const { code, state } = router.query;
 
-      // TODO: need to exit if code / state missing
+      const user = await storage.getUser();
+      if (!user) {
+        toast.error("User not found");
+        router.push("/profile");
+        return;
+      }
+
+      // TODO: need to exit if code / state missing -- add timer of some sort, after X seconds exits and error?
+
       if (code && state) {
         const codeStr = String(code);
         const stateStr = String(state);
 
-        const accessToken: AccessToken = await mintOAuthToken(codeStr, stateStr);
+        const accessToken: AccessToken | null = await mintOAuthToken(codeStr, stateStr);
         if (!accessToken) {
           toast.error("Unable to mint OAuth access token");
         } else {
-          toast.success("Successfully minted OAuth access token")
-          saveAccessToken(stateStr, accessToken.access_token);
+          saveAccessToken(stateStr, accessToken);
         }
+
+        if (accessToken) {
+          const mapping = OAUTH_APP_MAPPING[stateStr];
+          if (mapping) {
+            // TODO: Need better (1) chipIssuer selection when more communities are added, (2) Data Option selection when more import options are available
+            const leaderboardEntry = await importOAuthData(user.userData.username, ChipIssuer.EDGE_CITY_LANNA, accessToken, mapping.data_options[0]);
+
+            // TODO: Insert LeaderboardEntry into DB
+            console.log("Leaderboard Entry", leaderboardEntry);
+
+            toast.success("Successfully minted OAuth token and imported data!");
+          }
+        }
+
         router.push("/profile");
       }
     }
