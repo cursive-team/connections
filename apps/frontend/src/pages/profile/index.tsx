9071fba2d82a819f5ee9b02a7c976bd764de200a
AndrewCLu <andrewclu98@gmail.com>
2024-10-25 14:54:48 +0700

    update github link

--- apps/frontend/src/pages/profile/index.tsx
+++ apps/frontend/src/pages/profile/index.tsx
@@ -111,14 +111,17 @@ const ProfilePage: React.FC = () => {
                       addElement
                     />
                   </Link>
-                  <Tag
-                    emoji={<Icons.GitHub />}
-                    variant="gray"
-                    text="GitHub"
-                    className="min-w-max pl-4 pr-8"
-                    addElement
-                    onClick={() => toast.info("Coming soon!")}
-                  />
+                  <Link
+                    href={`https://www.github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&state=github&response_type=code&redirect_uri=${FRONTEND_URL}/oauth/exchange_token&approval_prompt=force&scope=read`}
+                  >
+                    <Tag
+                      emoji={<Icons.GitHub />}
+                      variant="gray"
+                      text="GitHub"
+                      className="min-w-max pl-4 pr-8"
+                      addElement
+                    />
+                  </Link>
                 </div>
               </div>
               {/* <Card.Base
