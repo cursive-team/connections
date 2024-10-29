e18b40aba5f9f90257e391c6b69e2b3100c98da7
Steven Elleman <steven.elleman@gmail.com>
2024-10-29 14:56:15 +0700

    messaging via websockets

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,9 +8,14 @@ import lannaRoutes from "./routes/lanna";
 import notificationRoutes from "./routes/notification";
 import dataHashRoutes from "./routes/dataHash";
 import { FRONTEND_URL } from "./constants";
-import { IntersectionState } from "@types";
-import { Controller } from "./lib/controller";
+import {
+  errorToString,
+  IntersectionState,
+} from "@types";
 const cors = require("cors");
+import { WebSocketServer, WebSocket } from 'ws';
+import { Controller } from "@/lib/controller";
+
 
 const app = express();
 const corsOptions = {
@@ -44,4 +49,92 @@ controller
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// TODO: move into new file and hoist here
+const WS_PORT = process.env.WS_PORT || 8090;
+
+const wsServer = new WebSocketServer({ port: Number(WS_PORT) })
+
+const clients: Record<string, WebSocket> = {};
+
+const handleError = (socket: WebSocket, target: string, error: string): void => {
+  const payload: WebSocketErrorPayload = {
+    error,
+  }
+
+  const resp: WebSocketResponse = {
+    type: WebSocketResponseTypes.ERROR,
+    targetSigPubKey: target,
+    senderSigPubKey: "",
+    payload
+  }
+
+  socket.send(JSON.stringify(resp))
+  return;
+}
+
+wsServer.on('connection', (socket: WebSocket) => {
+
+  socket.on('open', () => {
+    console.log("Open ws client connection")
+  });
+
+  socket.on('message', async (message: string) => {
+    const req: WebSocketRequest = WebSocketRequestSchema.parse(JSON.parse(message));
+
+    if (!req.authToken) {
+      return handleError(socket, req.senderSigPubKey, "Missing auth token.")
+    }
+
+    // Fetch user by auth token
+    // While the user isn't specifically required, it ensures the request is from an authenticated user
+    const user = await controller.GetUserByAuthToken(req.authToken);
+
+    if (!user) {
+      return handleError(socket, req.senderSigPubKey, "Invalid auth token.")
+    }
+
+    try {
+      switch (req.type) {
+        case WebSocketRequestTypes.CONNECT:
+          if (!req.senderSigPubKey) {
+            return handleError(socket, req.senderSigPubKey, "Missing target.")
+          }
+
+          // Set record for future lookup
+          clients[req.senderSigPubKey] = socket;
+
+          // NOTE: do I need to return response to signal successful connection?
+          return;
+        case WebSocketRequestTypes.CLOSE:
+          if (!req.senderSigPubKey) {
+            return handleError(socket, req.senderSigPubKey, "Missing target.")
+          }
+          delete clients[req.senderSigPubKey];
+          socket.close();
+          return;
+        case WebSocketRequestTypes.MSG:
+          if (!req.targetSigPubKey) {
+            return handleError(socket, req.senderSigPubKey, "Invalid target")
+          }
+
+          const resp: WebSocketResponse = MapRequestToResponse(req);
+          const stringResp: string = JSON.stringify(resp);
+
+          if (clients[req.targetSigPubKey]) {
+            clients[req.targetSigPubKey].send(stringResp);
+          }
+          return;
+        default:
+      };
+    } catch (error) {
+      return handleError(socket, req.senderSigPubKey, `Error handling message: ${errorToString(error)}`)
+      return;
+    };
+  });
+
+  socket.on('close', () => {
+    console.log(`Close ws client connection`);
+  });
 });
--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -11,6 +11,8 @@ export const FRONTEND_URL =
 export const BASE_API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
 
+export const BASE_API_WS = process.env.NEXT_PUBLIC_API_WS || "ws://localhost:8090/socket"
+
 export const OAUTH_APP_DETAILS: Record<string, OAuthAppDetails> = {
   strava: {
     client_side_fetching: true,
