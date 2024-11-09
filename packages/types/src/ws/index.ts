dfe1a6c67e718489a3d3ac13450f2f417bd59277
Steven Elleman <steven.elleman@gmail.com>
2024-11-10 04:33:43 +0700

    add untracked files, whoops

---
+++ packages/types/src/ws/index.ts
@@ -0,0 +1,74 @@
+import {z} from "zod";
+
+export enum SocketRequestType {
+  CONNECT = "CONNECT",
+  TAP_BACK = "TAP_BACK",
+  CLOSE = "CLOSE", // HERE: may not be necessary
+}
+
+export const SocketRequestTypeSchema = z.nativeEnum(SocketRequestType);
+
+export enum SocketResponseType {
+  SUCCESS_CONNECT = "CONNECT_SUCCESS",
+  TAP_BACK = "TAP_BACK",
+  ERROR = "ERROR"
+}
+
+export const SocketResponseTypeSchema = z.nativeEnum(SocketResponseType);
+
+
+export const SocketErrorPayloadSchema = z.object({
+  error: z.string(),
+});
+
+export type SocketErrorPayload = z.infer<
+  typeof SocketErrorPayloadSchema
+>;
+
+export const SocketRequestSchema = z.object({
+  authToken: z.string(),
+  type: SocketRequestTypeSchema,
+  senderSigPubKey: z.coerce.string().nullable(),
+  recipientSigPubKey: z.coerce.string().nullable(),
+  payload: z.any(),
+});
+
+export type SocketRequest = z.infer<
+  typeof SocketRequestSchema
+>;
+
+export const SocketResponseSchema = z.object({
+  type: SocketResponseTypeSchema,
+  senderSigPubKey: z.string().nullable(),
+  recipientSigPubKey: z.string().nullable(),
+  payload: z.any(),
+});
+
+export type SocketResponse = z.infer<
+  typeof SocketResponseSchema
+>;
+
+export const MapRequestToResponse = (req: SocketRequest): SocketResponse => {
+  let respType: SocketResponseType | null = null;
+
+  switch (req.type) {
+    case SocketRequestType.CONNECT:
+      respType = SocketResponseType.SUCCESS_CONNECT;
+      break;
+    case SocketRequestType.TAP_BACK:
+      respType = SocketResponseType.TAP_BACK;
+      break;
+    default:
+      throw new Error("Invalid socket request type");
+  }
+
+  if (!respType) {
+    throw new Error("Socket response type should not be null.")
+  }
+
+  return {
+    type: respType,
+    senderSigPubKey: req.senderSigPubKey,
+    recipientSigPubKey: req.recipientSigPubKey,
+  }
+}
