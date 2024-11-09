93260fac3b1653c0ea3cd915cddd9c6178bedfe3
Steven Elleman <steven.elleman@gmail.com>
2024-11-09 07:52:23 +0700

    Attempt to Convert Incorrect App Name to Correct One (#190)
    * attempt to convert incorrect app name into the correct one
    
    * cover all the cases

--- apps/frontend/src/lib/storage/types/user/oauth.ts
+++ apps/frontend/src/lib/storage/types/user/oauth.ts
@@ -1,4 +1,9 @@
-import { AccessTokenSchema, DataImportSourceSchema } from "@types";
+import {
+  AccessTokenSchema,
+  DataImportSource,
+  DataImportSourceSchema,
+  ImportDataType,
+} from "@types";
 import { z } from "zod";
 
 export const OAuthDataSchema = z.object({
@@ -9,7 +14,23 @@ export const OAuthDataSchema = z.object({
 export type OAuthData = z.infer<typeof OAuthDataSchema>;
 
 export const OAuthAppSchema = z.object({
-  app: DataImportSourceSchema,
+  app: z.coerce.string().transform((val) => {
+    // Context: For a multi hour period the DELETE_OAUTH backup type used the `data_options.type` rather than the app value. At the time we didn't check the type thoroughly. This transformation converts any of the `data_options.type` values to their associated app.
+    switch (val) {
+      case ImportDataType.STRAVA_PREVIOUS_MONTH_RUN_DISTANCE:
+        return DataImportSource.STRAVA;
+      case ImportDataType.GITHUB_LANNA_CONTRIBUTIONS:
+        return DataImportSource.GITHUB
+      case ImportDataType.GITHUB_CONTRIBUTIONS_LAST_YEAR:
+        return DataImportSource.GITHUB
+      case ImportDataType.GITHUB_STARRED_REPOS:
+        return DataImportSource.GITHUB
+      case ImportDataType.GITHUB_PROGRAMMING_LANGUAGES:
+        return DataImportSource.GITHUB
+      default:
+    }
+    return DataImportSourceSchema.parse(val);
+  })
 });
 
 export type OAuthApp = z.infer<typeof OAuthAppSchema>;
