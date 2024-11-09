5fb7f971ec73735609e078bed034a508de799de9
Steven Elleman <steven.elleman@gmail.com>
2024-11-09 07:43:20 +0700

    cover all the cases

--- apps/frontend/src/lib/storage/types/user/oauth.ts
+++ apps/frontend/src/lib/storage/types/user/oauth.ts
@@ -1,4 +1,9 @@
-import { AccessTokenSchema, DataImportSource, DataImportSourceSchema, LeaderboardEntryType } from "@types";
+import {
+  AccessTokenSchema,
+  DataImportSource,
+  DataImportSourceSchema,
+  ImportDataType,
+} from "@types";
 import { z } from "zod";
 
 export const OAuthDataSchema = z.object({
@@ -10,8 +15,19 @@ export type OAuthData = z.infer<typeof OAuthDataSchema>;
 
 export const OAuthAppSchema = z.object({
   app: z.coerce.string().transform((val) => {
-    if (val === LeaderboardEntryType.STRAVA_PREVIOUS_MONTH_RUN_DISTANCE) {
-      return DataImportSource.STRAVA;
+    // Context: For a multi hour period the DELETE_OAUTH backup type used the `data_options.type` rather than the app value. At the time we didn't check the type thoroughly. This transformation converts any of the `data_options.type` values to their associated app.
+    switch (val) {
+      case ImportDataType.STRAVA_PREVIOUS_MONTH_RUN_DISTANCE:
+        return DataImportSource.STRAVA;
+      case ImportDataType.GITHUB_LANNA_CONTRIBUTIONS:
+        return DataImportSource.GITHUB
+      case ImportDataType.GITHUB_CONTRIBUTIONS_LAST_YEAR:
+        return DataImportSource.GITHUB
+      case ImportDataType.GITHUB_STARRED_REPOS:
+        return DataImportSource.GITHUB
+      case ImportDataType.GITHUB_PROGRAMMING_LANGUAGES:
+        return DataImportSource.GITHUB
+      default:
     }
     return DataImportSourceSchema.parse(val);
   })
