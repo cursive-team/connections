726869b369fd5b7cfe40b7df8493f85c58de6ebd
Steven Elleman <steven.elleman@gmail.com>
2024-11-11 11:11:03 +0700

    socket server into express server file so global variables are available

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,16 +8,25 @@ import lannaRoutes from "./routes/lanna";
 import notificationRoutes from "./routes/notification";
 import dataHashRoutes from "./routes/dataHash";
 import { FRONTEND_URL } from "./constants";
+import { Server, Socket } from "socket.io";
+import {
+  SocketRequestType,
+  SocketRequestSchema,
+  SocketRequest,
+  SocketResponseType,
+  SocketErrorPayload,
+  errorToString,
+} from "@types";
+import {
+  User,
+} from "@/lib/controller/postgres/types";
 import { IntersectionState } from "@types";
 import { Controller } from "./lib/controller";
 const cors = require("cors");
 
 import * as http from 'http';
 
-// Hoist websockets
-import("./routes/socket");
-
-export const app = express();
+const app = express();
 const corsOptions = {
   origin: `${FRONTEND_URL}`,
 };
@@ -52,4 +61,115 @@ export const server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Socket Server:
+
+// Extend the interface to include User info -- with the User info attached we do not need sender info in request
+declare module 'socket.io' {
+  interface Socket {
+    user: User;
+  }
+}
+
+export const wsServer = new Server(server);
+
+// Middleware to authenticate the socket connection
+wsServer.use(async(socket, next) => {
+  const token = socket.handshake.auth.token;
+
+  if (!token) {
+    return next(new Error("Authentication error"));
+  }
+
+  // Fetch user by auth token
+  const user = await controller.GetUserByAuthToken(token);
+
+  if (!user) {
+    return next(new Error("Authentication error"));
+  } else {
+    // Attach user info to socket object
+    socket.user = user;
+    console.log("Set lookup")
+    app.locals.clientsSockets[user.signaturePublicKey] = socket.id;
+    console.log(app.locals.clientsSockets[user.signaturePublicKey])
+
+    return next();
+  }
+
+  return next(new Error("Authentication error"));
+});
+
+
+const handleError = (socket: Socket, error: string): void => {
+  const payload: SocketErrorPayload = {
+    error,
+  }
+
+  socket.emit(SocketResponseType.ERROR, payload)
+  return;
+}
+
+wsServer.on('connection', (socket: Socket) => {
+  socket.on('open', () => {
+    console.log("Open authenticated socket client connection.");
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Disconnect socket client connection.`);
+  });
+
+  socket.on('message', async (message: string) => {
+    console.log("Receive message.")
+    let sender = socket.user.signaturePublicKey;
+    let recipient: string | null = null;
+    try {
+      const req: SocketRequest = SocketRequestSchema.parse(JSON.parse(message));
+      console.log("Check req", sender, req)
+
+      recipient = req.recipientSigPubKey;
+      switch (req.type) {
+        case SocketRequestType.TAP_BACK:
+          console.log("TAP_BACK", recipient)
+          if (!recipient) {
+            return handleError(socket, "Missing recipient.");
+          }
+
+          console.log("Does socket exist: ", app.locals.clientsSockets[recipient])
+          if (app.locals.clientsSockets[recipient]) {
+            console.log(`Emit ${SocketRequestType.TAP_BACK} event.`)
+            const socketId = app.locals.clientsSockets[recipient];
+            socket.to(socketId).emit(SocketRequestType.TAP_BACK);
+          }
+          return;
+        case SocketRequestType.PSI:
+          console.log("PSI", recipient)
+          if (!recipient) {
+            return handleError(socket, "Missing recipient.");
+          }
+
+          console.log("Does socket exist: ", app.locals.clientsSockets[recipient])
+          if (app.locals.clientsSockets[recipient]) {
+            console.log(`Emit ${SocketRequestType.PSI} event.`)
+            const socketId = app.locals.clientsSockets[recipient];
+            socket.to(socketId).emit(SocketRequestType.PSI);
+          }
+          return;
+        case SocketRequestType.EXPUNGE:
+          console.log("Expunge socket info.")
+          delete app.locals.clientsSockets[sender];
+          socket.disconnect();
+          return;
+        default:
+          return;
+      };
+    } catch (error) {
+      const errMsg: string = `Error handling message: ${errorToString(error)}`;
+      console.error(errMsg);
+      if (sender) {
+        return handleError(socket, errMsg);
+      }
+      return;
+    }
+  });
 });
